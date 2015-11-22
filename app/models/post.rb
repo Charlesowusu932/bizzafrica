e1d49e97f721c353b4459fd64f36d764c3211147
Ernest Addae <earnestaddae@gmail.com>
2015-11-22 19:02:25 +0000

    Create post model, controller and views for index and show pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-
+validates :title, presence: true, length: {minimum: 10, maximum: 50}
+validates :summary, presence: true, length: {minimum: 30, maximum: 150}
+validates :description, presence: true
 end
