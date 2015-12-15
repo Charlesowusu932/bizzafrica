c41297dbc8d10fd78fcd917b22d4d72f827f2b5b
Ernest Addae <earnestaddae@gmail.com>
2015-12-15 21:24:42 +0000

    Style the post index page with homepage style

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,17 @@ class Post < ActiveRecord::Base
 validates :title, presence: true, length: {minimum: 10, maximum: 50}
 validates :summary, presence: true, length: {minimum: 30, maximum: 150}
 validates :description, presence: true, length: {minimum: 100}
+validate :picture_size
 
 mount_uploader :picture, PictureUploader
+
+
+
+ private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
