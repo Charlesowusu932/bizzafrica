c49c193ea5298390bd738640d4292fbf97f40544
Ernest Addae <earnestaddae@gmail.com>
2015-12-15 21:45:45 +0000

    Add static pages for form and writer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,12 @@ class PagesController < ApplicationController
   def home
   end
 
+  def writer
+
+  end
+
+  def form
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  root 'pages#home'
+ get 'writer', to: 'pages#writer'
+ get 'form', to: 'pages#form'
 
  resources :posts
 end
