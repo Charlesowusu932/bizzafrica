6826c90b5532001f98312267bd8512da013f3c4b
Ernest Addae <earnestaddae@gmail.com>
2015-12-15 17:49:56 +0000

    Create CRUD and form for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,43 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Your post was created successfully"
+      redirect_to posts_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = "Your post was updated successfully"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
+
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :summary, :description)
+    end
 end
